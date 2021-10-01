1f5d30e76d94369bebf4f76ca64453ba3b71e558
EvanDillon <evanjdillon@gmail.com>
2021-10-01 15:17:48 -0400

    SP2-C1 Refactored Accounts CRUD

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -31,6 +31,7 @@ class AccountsController < ApplicationController
   def create
     @account = Account.new(account_params)
     @account.user_id = current_user.id
+    @account.balance = calculate_balance(@account)
 
     if @account.save
       redirect_to accounts_path, success: "Account was successfully created."
@@ -43,7 +44,9 @@ class AccountsController < ApplicationController
   # PATCH/PUT /accounts/1 or /accounts/1.json
   def update
     if @account.update(account_params)
-      redirect_to accounts_path, success: "Account was successfully updated."
+      @account.balance = calculate_balance(@account)
+      @account.save
+      redirect_to accounts_path, success: ErrorMessage.find_by(error_name: "account_updated").body
     else
       redirect_to edit_account_path(@account), danger: "#{@account.errors.first.full_message}"
     end
@@ -65,5 +68,9 @@ class AccountsController < ApplicationController
     # Only allow a list of trusted parameters through.
     def account_params
       params.fetch(:account, {}).permit!
+    end
+
+    def calculate_balance(account)
+      account.initial_balance + (account.debit - account.credit)
     end
 end
