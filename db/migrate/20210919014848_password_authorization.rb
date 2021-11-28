c5a6aef7930cb4ffb20903bf8646e9827c6007b2
EvanDillon <evanjdillon@gmail.com>
2021-11-28 18:58:09 -0500

    SP5 - Don't ask

--- db/migrate/20210919014848_password_authorization.rb
+++ db/migrate/20210919014848_password_authorization.rb
@@ -2,7 +2,7 @@ class PasswordAuthorization < ActiveRecord::Migration[6.1]
   def change
     create_table :password_authorizations do |t|
       t.references :user, foreign_key: true
-      t.references :password_question, foreign_key: true
+      t.references :security_questions, foreign_key: true
       t.text :answer
 
       t.timestamps null: false
