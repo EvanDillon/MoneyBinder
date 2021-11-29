403492ff48fbfd0fd8018e391a20d02853c0393c
EvanDillon <evanjdillon@gmail.com>
2021-11-29 12:23:19 -0500

    SP5 - I am dying

--- db/migrate/20211129163903_change_security_q_name.rb
+++ db/migrate/20211129163903_change_security_q_name.rb
@@ -1,5 +1,7 @@
 class ChangeSecurityQName < ActiveRecord::Migration[6.1]
-  def change
-    rename_column :password_join_authorizations, :security_question_id, :security_questions_id
+  if Rails.env != 'production'
+    def change
+      rename_column :password_join_authorizations, :security_question_id, :security_questions_id
+    end
   end
 end
