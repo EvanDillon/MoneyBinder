956c5d9defa1ff23519ed4b15511e721c8561005
EvanDillon <evanjdillon@gmail.com>
2021-11-29 15:38:12 -0500

    SP5 - Fix mailer in prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,6 +40,18 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => 'moneybinder.herokuapp.com' }
+
+  config.action_mailer.smtp_settings = {
+    :address                => 'smtp.gmail.com',
+    :port                   => 587,
+    :user_name              => 'moneybinder21@gmail.com',
+    :password               => 'qnvwvrzzycpovewc',
+    :authentication         => 'plain',
+    :enable_starttls_auto   => true
+  }
+
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
